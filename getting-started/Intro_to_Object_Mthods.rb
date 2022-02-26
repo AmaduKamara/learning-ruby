2662a32851ca8151901427a8ab0dc931059de658
Amkam <amaduskamara@gmail.com>
2022-02-26 14:57:24 +0000

    About object methods and invocation

---
+++ getting-started/Intro_to_Object_Mthods.rb
@@ -0,0 +1,17 @@
+# Use the dot(.) to invoke a method
+p "Hello World!".length # 12
+puts "Samuel Amkam".length # 12
+puts "Samuel Amkam".upcase # prints in uppercase
+puts "HELLO WORLD".downcase # prints in lowercase
+
+student_name = 'Web Avenger'
+puts student_name.upcase
+puts student_name.downcase
+puts student_name.capitalize # prints first letter in capitals
+
+p 10
+p 10.next # eleven
+p -1.next
+
+puts "Hello World!"
+puts "Hello World!".inspect
