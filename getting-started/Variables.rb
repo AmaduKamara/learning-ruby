3ae103db24637bfe69d09ed78072bcf563f427a9
Amkam <amaduskamara@gmail.com>
2022-02-26 14:02:22 +0000

    Variables

---
+++ getting-started/Variables.rb
@@ -0,0 +1,20 @@
+name = "Samuel Amkam"
+full_name = "Amadu Kamara"
+github_name = "AmaduKamara"
+twitter_name = "DevAmkam"
+current_money = "Le 20,000"
+
+first_name = "Amadu"
+last_name = "Kamara"
+
+puts "My name is " + name 
+
+puts "My full name is " + full_name
+
+puts "My Github name is " + github_name
+
+puts "My Twitter name is " + twitter_name
+
+puts "I current own " + current_money
+
+puts first_name + " " + last_name
