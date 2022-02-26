51dad16f8284d2f4f0c4da78e6596ddafd34be50
Amkam <amaduskamara@gmail.com>
2022-02-26 15:48:17 +0000

    Numbers and Booleans - an intro

---
+++ numbers_booleans/Intro_to_Numbers.rb
@@ -0,0 +1,13 @@
+# There are two types of numbers in Ruby 
+# Integers = 10 whole numbers
+# Booleans 10.5 flaoting points or decimal numbers
+
+p 5.class
+p 0.next.class
+p 100
+p -832
+
+
+p 5.4.class
+p 2.5.class
+p -10.23
