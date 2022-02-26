dcf7840828469d562b249ea5927ca2c2538536eb
Amkam <amaduskamara@gmail.com>
2022-02-26 17:07:17 +0000

    Equality and Inequality operators

---
+++ numbers_booleans/Equality_and_Inequality _Operator.rb
@@ -0,0 +1,14 @@
+p 10 == 10
+p 2 != 4
+p 4 == 5
+
+maryAge = 24
+abuAge = 30
+
+p maryAge == abuAge
+
+p "4" == "4"
+
+p "5".to_f === 5.0
+p "Hello" == "hello"
+p "Hello".downcase == "hello".downcase
