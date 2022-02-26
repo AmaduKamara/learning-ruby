9962f987d16d815607f8b1de380a66a7e10de541
Amkam <amaduskamara@gmail.com>
2022-02-26 15:19:28 +0000

    String interpolation

---
+++ getting-started/String_Interpolation.rb
@@ -0,0 +1,17 @@
+university_name = "Limkokwing University"
+
+puts "I attend the #{university_name} of creative Technology" # interpolating the variable inside the string texts
+
+p 5
+p 5.next
+p 10.to_s # converts the number to a string
+
+name = "Amkam"
+p "My name is #{name} and I attend the #{university_name} of creative Technology" # interpolating the variable inside the
+
+age = 54
+
+puts "I am #{age} or " + age.next.to_s + " years old"
+p "The result of adding 3 + 5 is #{5 + 5}"
+
+p "In #{2.to_s} year time, I'll have a big software application running in my country solving great problem for my people"
