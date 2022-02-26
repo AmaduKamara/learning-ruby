9e23fa5aa7b22e2634b88fa74f2910e4280a3f69
Amkam <amaduskamara@gmail.com>
2022-02-26 16:54:07 +0000

    Boolean methods even and odd

---
+++ numbers_booleans/Boolean_Odd_Even.rb
@@ -0,0 +1,12 @@
+# The odd and even methods checks if a value is odd or even and returns true or false
+# use the question ? mark at the end of a boolean method to check for true or false
+p 3.odd?
+p 5.odd?
+
+p 4.even?
+
+p 2.even?
+p 3.even?
+p 13.even?
+
+# floating point niumbers does not have booleans methods
