3186efac3593141b1dcbfb0e9c437cccc907e50e
Amkam <amaduskamara@gmail.com>
2022-02-26 15:36:41 +0000

    Gets method for keyboard inputs and interpolation

---
+++ getting-started/Gets.rb
@@ -0,0 +1,10 @@
+puts "What is you name: "
+name = gets
+puts "What is your email: "
+email = gets
+puts "What is your age: "
+age = gets.chomp.to_s
+puts "What university you attend: "
+university = gets.chomp
+
+print "My name is #{name} my email is #{email} and I attend #{university} and I am #{age} years old."
