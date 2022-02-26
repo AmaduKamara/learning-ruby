376011142ca857a38736ef6cc1621a6d9a608cce
Amkam <amaduskamara@gmail.com>
2022-02-26 13:21:28 +0000

    The p method

---
+++ getting-started/P.rb
@@ -0,0 +1,10 @@
+puts "Web Avenger"
+
+# The p method gives description of what it prints to the screen
+p "Samuel Amkam"
+
+puts "Hi there, this is a
+big line break"
+
+p "Hi there, this a
+big line break"
