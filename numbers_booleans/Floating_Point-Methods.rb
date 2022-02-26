067bb70ca0653ec79e7ff93d4b933ed7a4ad0ddc
Amkam <amaduskamara@gmail.com>
2022-02-26 19:27:17 +0000

    Floating point methods

---
+++ numbers_booleans/Floating_Point-Methods.rb
@@ -0,0 +1,15 @@
+p 10.5.to_i
+p 10.5.to_i.class
+
+p 10.5.floor
+p 10.5023.floor(2)
+p 10.5.ceil
+
+p 3.14159.round
+p 3.54159.round
+p 3.14159.round(2)
+p 3.14159.round(3)
+p 3.14159.round(4)
+
+p 35.67.abs
+p -35.67.abs
