da6737c23dc237ebbf60fa86ec581f73fb712d89
Amkam <amaduskamara@gmail.com>
2022-02-26 13:14:01 +0000

    Learned about the puts method

---
+++ getting-started/Puts.rb
@@ -0,0 +1,14 @@
+puts "Hello World!"
+puts
+puts "I am learning Ruby Language"
+puts
+puts "Puts adds a line break in your output of the program"
+puts
+puts "The puts is an example of a statement in Ruby"
+
+puts 4.5 # Floating point
+puts 10 # Integer
+puts "This a string" # String
+
+puts 4 + 3
+puts "4" + "3" # Append and concatenation
