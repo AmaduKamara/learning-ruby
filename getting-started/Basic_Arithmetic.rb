7cfb1798da433c537cee9303fb7120420341639a
Amkam <amaduskamara@gmail.com>
2022-02-26 13:32:09 +0000

    Basic arithmetics

---
+++ getting-started/Basic_Arithmetic.rb
@@ -0,0 +1,27 @@
+p 1 + 4 # 5
+
+p 10 - 4 # 6
+
+p 3 * 3 # 9 
+
+p 10 / 2 # 5
+
+
+p "Samuel" + " " + "Amkam" # prints a string in a quote
+puts "Amkam" + " " + "Web Avenger"
+
+p 10 / 5
+p 12 / 5
+p 12.0 / 5 # prints a float 2.4
+
+# Exponential notation
+p 2 * 2 * 2 * 2
+p 2 ** 5 # 32
+p 3 ** 3 # 27
+
+p 5 % 2 # Modulo = 1 = returns the reminder of a division
+p 10 % 2 # 0
+p 14 % 4 # 2
+
+p 0.5 / 2 # 0.25
+p -0.75 / 2
