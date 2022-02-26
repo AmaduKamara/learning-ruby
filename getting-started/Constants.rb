2de57d551d0dc65c5a67903566bd3fd362aa052c
Amkam <amaduskamara@gmail.com>
2022-02-26 14:47:35 +0000

    Constants

---
+++ getting-started/Constants.rb
@@ -0,0 +1,9 @@
+age = 32
+p age
+
+PLACE_OF_BIRTH = "Sierra Leone"
+p PLACE_OF_BIRTH
+puts PLACE_OF_BIRTH
+
+NAME = "Amadu Kamara"
+puts "My name is #{NAME}"
---
+++ getting-started/Parallel_Variable_Assignment.rb
@@ -0,0 +1,25 @@
+# Parallel Variable Assignment
+
+# a = 10
+# b = 20
+# c = 30
+
+a, b, c = 10, 20, 30 # assignment happens sequentially
+
+p a, b, c
+
+# p a
+# p b
+# p c
+
+# puts a, b, c
+# print a, b, c
+
+va = 1 
+
+vb = 2
+
+va, vb = vb, va
+
+p va, vb
+
