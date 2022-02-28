6127fa0e2cfe157d8a8ebb90222741d1ab4af4ea
Amkam <amaduskamara@gmail.com>
2022-02-28 09:36:58 +0000

    Intro to Mthods in Ruby

---
+++ methods/Intro_to_Mthods.rb
@@ -0,0 +1,9 @@
+def introduce_myself
+  puts "Hello, I am Amadu Kamara"
+  puts "I am a talented Web Developer"
+  puts "I am handsome, fun and friendly"
+  puts
+end
+
+introduce_myself
+introduce_myself
