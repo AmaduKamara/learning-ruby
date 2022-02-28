e3ad6449fd1e656fdff87c65590197d074dad67b
Amkam <amaduskamara@gmail.com>
2022-02-28 09:41:28 +0000

    Local variables in methods

---
+++ methods/Local_Variables.rb
@@ -0,0 +1,8 @@
+# A local variable is something that is confined in a instance
+# Local variables are scoped to a function, conditional blockes, and classes
+def introduce_myself
+  expression = "I am a talented web developer"
+  puts expression
+end
+
+introduce_myself
