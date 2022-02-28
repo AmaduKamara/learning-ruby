5bdf3d98696beac0f6e6f283a49511bd86de2aea
Amkam <amaduskamara@gmail.com>
2022-02-28 09:50:54 +0000

    Methods with parameters

---
+++ methods/Parameters_and_Arguments.rb
@@ -0,0 +1,9 @@
+
+def praise_person(name, age)
+  puts "#{name}, #{age} is a talented web developer"
+end
+
+praise_person("Samuel Amkam", 26)
+praise_person("WebAvenger", 26)
+praise_person("Amadu Kamara", 26)
+praise_person("Amkam", 32)
