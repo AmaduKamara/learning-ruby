bb4e3d8ab08e19fa0fef37e870bd93f2c49e9e83
Amkam <amaduskamara@gmail.com>
2022-03-01 07:56:28 +0000

    Test ternary

--- methods/Ternary_Operator.rb
+++ methods/Ternary_Operator.rb
@@ -3,3 +3,7 @@ puts 4 > 10 ? "Yes it is" : "No it's not"
 pokemon = "Charizard"
 
 puts pokemon == "Charizard" ? "Fireball!" : "That is not Charizard"
+
+name = "Ranjeet"
+
+puts name == "Ranjeet" ? "Yes you're" : "No you are not"
